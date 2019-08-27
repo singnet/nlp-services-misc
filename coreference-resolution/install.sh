5c30e7f924d21c4605a708ff007eea966a331f8c
Eskender <eskendertamrat52@gmail.com>
2019-08-27 16:52:52 +0300

    changes for beta-v2 testing

--- coreference-resolution/install.sh
+++ coreference-resolution/install.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 
 snet_daemon_v=1.0.0
+snet_daemon_v_2=2.0.2
 
 if [ ! -d models/ ]; then
     mkdir models
     wget https://s3-us-west-2.amazonaws.com/allennlp/models/coref-model-2018.02.05.tar.gz -O models/coref-model-2018.02.05.tar.gz
 fi
+
 python3.6 -m spacy download en_core_web_sm
 
 # apt install tar
 if [ ! -d snet-daemon-v$snet_daemon_v ] ; then
-	echo "Downloading snetd-linux"
+        echo "Downloading snetd-linux"
 	wget https://github.com/singnet/snet-daemon/releases/download/v$snet_daemon_v/snet-daemon-v$snet_daemon_v-linux-amd64.tar.gz
 	tar -xzf snet-daemon-v$snet_daemon_v-linux-amd64.tar.gz
 	ln snet-daemon-v$snet_daemon_v-linux-amd64/snetd snetd-linux-amd64
 	rm snet-daemon-v$snet_daemon_v-linux-amd64.tar.gz
+        wget https://github.com/singnet/snet-daemon/releases/download/v$snet_daemon_v_2/snet-daemon-v$snet_daemon_v_2-linux-amd64.tar.gz
+        tar -xzf snet-daemon-v$snet_daemon_v_2-linux-amd64.tar.gz
+        ln snet-daemon-v$snet_daemon_v_2-linux-amd64/snetd snetd-linux-amd64-2
+        rm snet-daemon-v$snet_daemon_v_2-linux-amd64.tar.gz
 else
 	echo "Folder seems to exist"
 fi
@@ -22,4 +28,5 @@ fi
 python3.6 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. service_spec/CoreferenceResolutionService.proto
 
 cp snet.config.example.mainnet snet.config.example.mainnet.json
+cp snet.config.example.mainnet_2 snet.config.example.mainnet_2.json
 cp snet.config.example.ropsten snet.config.example.ropsten.json
--- named-entity-disambiguation/install.sh
+++ named-entity-disambiguation/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
 snet_daemon_v=1.0.0
+snet_daemon_v_2=2.0.2
 
 if [ ! -d snet-daemon-v$snet_daemon_v ] ; then
 	echo "Downloading snetd-linux"
@@ -8,14 +8,18 @@ if [ ! -d snet-daemon-v$snet_daemon_v ] ; then
 	tar -xzf snet-daemon-v$snet_daemon_v-linux-amd64.tar.gz
 	ln snet-daemon-v$snet_daemon_v-linux-amd64/snetd snetd-linux-amd64
 	rm snet-daemon-v$snet_daemon_v-linux-amd64.tar.gz
+	wget https://github.com/singnet/snet-daemon/releases/download/v$snet_daemon_v_2/snet-daemon-v$snet_daemon_v_2-linux-amd64.tar.gz
+       	tar -xzf snet-daemon-v$snet_daemon_v_2-linux-amd64.tar.gz
+       	ln snet-daemon-v$snet_daemon_v_2-linux-amd64/snetd snetd-linux-amd64-2
+       	rm snet-daemon-v$snet_daemon_v_2-linux-amd64.tar.gz
 else
 	echo "Folder seems to exist"
 fi
-
 
 # Create dictionary for the available service.
 python3.6 service/dictionary_creator.py
 python3.6 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. service_spec/NamedEntityDisambiguation.proto
 
 cp snet.config.example.mainnet snet.config.example.mainnet.json
+cp snet.config.example.mainnet_2 snet.config.example.mainnet_2.json
 cp snet.config.example.ropsten snet.config.example.ropsten.json
